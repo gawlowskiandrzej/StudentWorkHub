9b8f9adf8a7b3eff75e26b50c8c1fa2c84d28b95
gawlowskiandrzej <andrzejoklol@gmail.com>
2025-12-23 14:06:30 +0100

    details view scheme

--- frontend/components/feature/list/ListElement.tsx
+++ frontend/components/feature/list/ListElement.tsx
@@ -1,8 +1,14 @@
 "use client";
+import { Progress } from '@/components/ui/progress';
+import { useRouter } from 'next/navigation';
+export function ListElement({key}: {key: number}) {
+    const router = useRouter();
+    function goToDetails(key: number): void {
+        router.push('/details/' + key);
+    }
 
-export function ListElement() {
     return (
-            <div className="offer-list-header cursor-pointer scale-hover">
+            <div className="offer-list-header cursor-pointer scale-hover" onClick={() => goToDetails(key)}>
                 <img className="company" src="/icons/company0.svg" />
                 <div className="offer-header">
                     <div className="offer-header-info">
@@ -27,8 +33,8 @@ export function ListElement() {
                         </div>
                     </div>
                     <div className="offer-header-salary-date">
-                        <div className="added-10-12-2025">Added 10.12.2025</div>
                         <div className="job-salary">Job salary</div>
+                        <div className="added-10-12-2025 flex gap-5"><div>10.12.2025</div><Progress className='w-[100px]' value={10} max={100} /><div>12.12.2025</div></div>
                     </div>
                     </div>
                 </div>
