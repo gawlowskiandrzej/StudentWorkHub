1188f78808c28642b82a76d6fd72c777af9f579f
gawlowskiandrzej <andrzejoklol@gmail.com>
2026-01-10 12:00:58 +0100

    styles changes

--- frontend/components/profileCreation/steps/stepMajor.tsx
+++ frontend/components/profileCreation/steps/stepMajor.tsx
@@ -2,8 +2,11 @@
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { StepProps } from "@/constants/profileCreation/steps";
 import styles from '@/styles/ProfileCreationStyle.module.css';
+import buttonStyles from '@/styles/ButtonStyle.module.css'
+import searchStyles from '@/styles/SearchView.module.css'
 import { useTranslation } from "react-i18next";
 import { useProfileCreationDictionaries } from "@/hooks/useDictionaries";
+import { SelectGroup, SelectLabel } from "@radix-ui/react-select";
 
 export default function StepMajor({ data, updateData }: StepProps) {
 
@@ -38,22 +41,29 @@ export default function StepMajor({ data, updateData }: StepProps) {
           <label className={styles.inputLabel}>
             {t("selectYourPreferredMajor")} <span className={styles.requiredStar}>*</span>
           </label>
+          <div className={`${searchStyles["major-study-search"]} ${buttonStyles["floating-input"]}`}>
           <Select
+          
             value={data.major ? String(data.major) : ""}
             onValueChange={(val) => updateData("major", Number(val))}
           >
-            <SelectTrigger className={styles.unifiedInput}>
+            <SelectTrigger color="secondary" className={`cursor-pointer border-0 m-0 w-full`}>
               <SelectValue placeholder={t("majorPlaceholder")} />
             </SelectTrigger>
 
-            <SelectContent className={styles.unifiedSelectContent}>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel className="text-primary">{t("majorPlaceholder")}</SelectLabel>
+              
               {fullDictionaries.leading_categories.map((category) => (
-                <SelectItem key={category.id} value={String(category.id)} className={styles.unifiedSelectItem}>
+                <SelectItem key={category.id} value={String(category.id)}>
                   {category.name}
                 </SelectItem>
               ))}
+              </SelectGroup>
             </SelectContent>
           </Select>
+          </div>
         </div>
 
       </div>
