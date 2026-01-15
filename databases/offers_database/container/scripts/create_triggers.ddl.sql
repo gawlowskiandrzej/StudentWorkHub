c3aa46896cae5aea74d1698662cdcef709fd5d46
Mateusz <chwistekmat@gmail.com>
2026-01-15 13:50:20 +0100

    Update trigger rules

--- databases/offers_database/container/scripts/create_triggers.ddl.sql
+++ databases/offers_database/container/scripts/create_triggers.ddl.sql
@@ -1,6 +1,6 @@
 DROP TRIGGER IF EXISTS trg_offers_cleanup_expired ON public.offers;
 
-CREATE TRIGGER trg_offers_cleanup_expired_upd
-    AFTER UPDATE OF expires ON public.offers
+CREATE TRIGGER trg_offers_cleanup_expired
+    AFTER INSERT OR UPDATE ON public.offers
         FOR EACH STATEMENT
             EXECUTE FUNCTION public.offers_cleanup_expired();
