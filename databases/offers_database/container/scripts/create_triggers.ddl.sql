9e16f61c0a8e308bf93c6890dd2eb78be5a6fe0d
Mateusz-Chwistek <chwistekmat@gmail.com>
2026-01-26 21:46:37 +0100

    Delete triggers script

--- databases/offers_database/container/scripts/create_triggers.ddl.sql
+++
@@ -1,6 +0,0 @@
-DROP TRIGGER IF EXISTS trg_offers_cleanup_expired ON public.offers;
-
-CREATE TRIGGER trg_offers_cleanup_expired
-    AFTER INSERT OR UPDATE ON public.offers
-        FOR EACH STATEMENT
-            EXECUTE FUNCTION public.offers_cleanup_expired();
